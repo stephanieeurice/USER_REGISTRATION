83d4124fe20e3d3b2a488226ad3b19bf7a51f2cc
stephanieeurice <stephanieeurice1800@gmail.com>
2021-09-11 21:06:39 +0800

    Added User list and Edit User Information Tab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
+import {UserList} from './UserList'
+import {EditUser} from './EditUser'
+import {UserReg} from './UserReg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+                <nav className="navbar bg-light navbar-expand-lg navbar-light">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">User List</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/registration" className="nav-link">Register User</Link>
+                        </li>
+                    </ul>
+                </nav>
+             <Switch>
+                <Route exact path = "/" component = {UserList}/>
+                <Route path="/edit/:id" component={EditUser}/>
+                <Route path="/registration" component={UserReg}/>
+             </Switch>
+        </div>
   );
 }
 
---
+++ src/UserReg.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UserReg = () => {
+    return <div className = "container"> Register User</div>
+}
