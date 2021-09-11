7f7757abf19ff7009b7721992afd7feed39eda90
stephanieeurice <stephanieeurice1800@gmail.com>
2021-09-12 02:58:12 +0800

    Added form and Edit

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
+import { getUsers } from './api'
+import { FaEdit } from 'react-icons/fa';
+import './style.css'
+
 
 export const UserList = () => {
-    return <div className = "container"> User List</div>
+
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const users = await getUsers()
+            setUsers(users)
+        }
+        fetchUsers()
+    }, [])
+
+
+    return (
+    <div className = "container"> 
+        <div className = "mt-3">
+            <h3> User List</h3>
+            <table className = "table table-striped mt-3">
+                <thead>
+                    <tr>
+                        <th> Last Name </th>
+                        <th> First Name </th>
+                        <th> Middle Name </th>
+                        <th> Age </th>
+                        <th> Gender </th>
+                        <th> Address </th>
+                        <th> Email </th>
+                        <th> Contact No. </th>
+                        <th> Action </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(user => {
+                            return (
+                                <tr key={user._id}>
+                                       
+                                        <td>{user.lastName}</td>
+                                        <td>{user.firstName}</td>
+                                        <td>{user.middleName}</td>
+                                        <td>{user.age}</td>
+                                        <td>{user.gender}</td>
+                                        <td>{user.address}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.number}</td>
+                                        <td>
+                                            <Link to={`/edit/${user._id}`} ><FaEdit style={{ color: '#363738' }} /></Link>
+                                        </td>
+                                    </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </div>
+    )
 }
